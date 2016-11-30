85476e681862019d996fb69604f855c95d7e7603
Gaurav <Gaurav@DESKTOP-8QU0EUE>
2016-11-30 15:01:00 -0500

    Gaurav 1

--- EcomApp/public/javascripts/global.js
+++ EcomApp/public/javascripts/global.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
     // Populate the user table on initial page load
     populateTable();
-    populateuserTable();
+    //populateuserTable();
     topproductpopulateTable();
 
 });
@@ -37,28 +37,28 @@ function populateTable() {
 };
 
 
-// Fill table with data
-function populateuserTable() {
-
-    // Empty content string
-    var tableContent = '';
-
-    // jQuery AJAX call for JSON
-    $.getJSON('/users/userlist', function (data) {
-
-        // For each item in our JSON, add a table row and cells to the content string
-        $.each(data, function () {
-            tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
-            tableContent += '<td>' + this.email + '</td>';
-            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
-            tableContent += '</tr>';
-        });
-
-        // Inject the whole content string into our existing HTML table
-        $('#userList table tbody').html(tableContent);
-    });
-};
+//// Fill table with data
+//function populateuserTable() {
+//
+//    // Empty content string
+//    var tableContent = '';
+//
+//    // jQuery AJAX call for JSON
+//    $.getJSON('/users/userlist', function (data) {
+//
+//        // For each item in our JSON, add a table row and cells to the content string
+//        $.each(data, function () {
+//            tableContent += '<tr>';
+//            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
+//            tableContent += '<td>' + this.email + '</td>';
+//            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+//            tableContent += '</tr>';
+//        });
+//
+//        // Inject the whole content string into our existing HTML table
+//        $('#userList table tbody').html(tableContent);
+//    });
+//};
 
 
 // Fill table with data
--- EcomApp/routes/products.js
+++ EcomApp/routes/products.js
@@ -1,12 +1,37 @@
 ﻿var express = require('express');
 var router = express.Router();
 
+var redis = require("redis");
+
+// Add your cache name and access key.
+var client = redis.createClient(6380, 'cacheISM6930.redis.cache.windows.net', { auth_pass: 'EH6e/d9G7VA5vmStzXmsh0Sq0ypDF3OBRBKukGVVhz0=', tls: { servername: 'cacheISM6930.redis.cache.windows.net' } });
+
+client.set("key1", "value", function (err, reply) {
+    console.log(reply);
+});
+
+client.get("key1", function (err, reply) {
+    console.log(reply);
+});
+
+client.hmset("tools", "webserver", "expressjs", "database", "mongoDB", "devops", "jenkins", function (err, reply) {
+    console.log(err);
+    console.log(reply);
+});
+
+client.hgetall("tools", function (err, reply) {
+    console.log(err);
+    console.log(reply);
+});
+
+
+
 /*
- * GET userlist.
+ * GET productlist.
  */
 router.get('/productlist', function (req, res) {
     var db = req.db;
-    var collection = db.get('Product');
+    var collection = db.get('Products');
     collection.find({}, {}, function (e, docs) {
         res.json(docs);
     });
@@ -15,8 +40,8 @@ router.get('/productlist', function (req, res) {
 
 router.get('/topproductlist', function (req, res) {
     var db = req.db;
-    var collection = db.get('Product');
-    collection.find({"_id": "583e2545c9ee4c26f02e3917"}, {}, function (e, docs) {
+    var collection = db.get('Products');
+    collection.find({ "CATEGORY": "Cell Phones & Accessories"}, {}, function (e, docs) {
         res.json(docs);
     });
 });
